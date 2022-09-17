83f38ca46e7dbba84dbfa82a425c9070b04cf313
AliceWu78 <alice.wu@envoy.com>
2022-09-17 10:54:43 -0700

    Add Map to campground

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,39 @@
+// const campground = require("../../models/campground");
+
+
+// mapboxgl.accessToken = mapToken;
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//     center: campground.geometry.coordinates,// starting position [lng, lat]
+//     zoom: 8, // starting zoom
+//     projection: 'globe' // display the map as a 3D globe
+// });
+
+// console.log(campground)
+
+mapboxgl.accessToken=mapToken;
+const map=new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
+    projection: 'globe' // display the map as a 3D globe
+});
+
+map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+});
+
+// Create a default Marker and add it to the map.
+const marker=new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h4>${campground.title}</h4><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map);
